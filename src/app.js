374c50df9336bce5380eb70ff0ae284ca43a802f
Victor Serpa do Carmo <victor.serpa.c@icloud.com>
2017-11-03 12:08:40 -0200

    Estruturar base da aplicação

---
+++ src/app.js
@@ -0,0 +1,60 @@
+/* .env lib */
+require('dotenv').config();
+
+/* Dependencies */
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const i18n = require('./config/i18n');
+
+/* Routes */
+const alunoRoutes = require('./routes/aluno');
+const mediaRoutes = require('./routes/media');
+const filmeRoutes = require('./routes/filme');
+const livroRoutes = require('./routes/livro');
+
+/* Express initialization */
+const app = express();
+
+/* Logger */
+const LoggerConfig = require('./config/LoggerConfig');
+const Logger = require('./helpers/Logger');
+
+/* Express utilites */
+app.use(helmet());
+app.use(cors());
+app.use(compression());
+app.use(i18n.init);
+app.use(bodyParser.json({
+  limit: process.env.BODY_LIMIT,
+}));
+
+/* Log express request and response */
+LoggerConfig.expressRequest(app);
+
+/* Status endpoint */
+app.get('/', (req, res) => {
+  res.send('ok');
+});
+
+/* Instatiate routes */
+app.use('/aluno', alunoRoutes);
+// app.use('/media', mediaRoutes);
+app.use('/filme', filmeRoutes);
+// app.use('/livro', livroRoutes);
+
+/* Log errors */
+LoggerConfig.expressError(app);
+
+app.all('*', (req, res) => {
+  res.status(404).send({ success: false, code: '404' });
+});
+
+/* Startup message */
+app.listen(process.env.PORT, () => {
+  /* Configure Log */
+  LoggerConfig.init();
+  Logger.info(`Server started on port ${process.env.PORT}`);
+});
